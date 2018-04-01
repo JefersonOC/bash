beb02b63fe96650305748d289b437465530544c0
Jeferson Cruz <jeferson.hck@gmail.com>
2018-04-01 18:29:30 -0300

    Update uicustomization.sh

--- ubuntu-dev-tools/extra/uicustomization.sh
+++ ubuntu-dev-tools/extra/uicustomization.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
 
 echo 'Adding tools and repository for new themes and icons...'
-add-apt-repository -y ppa:noobslab/themes
-add-apt-repository -y ppa:noobslab/icons
-apt-get update
-apt-get install -y compizconfig-settings-manager \
+sudo add-apt-repository -y ppa:noobslab/themes
+sudo add-apt-repository -y ppa:noobslab/icons
+sudo apt-get update
+sudo apt-get install -y compizconfig-settings-manager \
                    compiz-plugins \
                    myelementary
 cd ~ && git clone https://github.com/horst3180/arc-theme --depth 1 && cd arc-theme
 ./autogen.sh --prefix=/usr
 make install
 rm -rf ~/arc-theme
+
+sudo add-apt-repository ppa:numix/ppa
+sudo apt-get update
+sudo apt-get install -y numix-gtk-theme numix-icon-theme-circle
+
 echo 'Is now possible to customize the ubuntu GUI'
