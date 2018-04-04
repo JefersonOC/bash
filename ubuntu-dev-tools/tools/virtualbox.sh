7b6555ceab791c2c5f195ed4d70e610e18a47dc7
Jeferson Cruz <jeferson.hck@gmail.com>
2018-04-04 09:17:50 -0300

    Update virtualbox.sh

--- ubuntu-dev-tools/tools/virtualbox.sh
+++ ubuntu-dev-tools/tools/virtualbox.sh
@@ -1,3 +1,3 @@
-wget https://download.virtualbox.org/virtualbox/5.2.8/VirtualBox-5.2.8-121009-Linux_amd64.run -O virtualbox.run
-chmod +x virtualbox.run
+wget https://download.virtualbox.org/virtualbox/5.2.8/VirtualBox-5.2.8-121009-Linux_amd64.run -O virtualbox.run &&
+chmod +x virtualbox.run &&
 sudo ./virtualbox.run
