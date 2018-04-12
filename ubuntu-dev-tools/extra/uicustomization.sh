ed322e82bf65503b5f8e2aa2cf0bb939cb20aea3
Jeferson Cruz <jeferson.hck@gmail.com>
2018-04-11 21:14:14 -0300

    Update uicustomization.sh

--- ubuntu-dev-tools/extra/uicustomization.sh
+++ ubuntu-dev-tools/extra/uicustomization.sh
@@ -6,4 +6,17 @@ sudo add-apt-repository ppa:numix/ppa &&
 sudo apt-get update &&
 sudo apt-get install -y numix-gtk-theme numix-icon-theme-circle numix-icon-theme-square
 
+echo 'small header bar ...'
+tee ~/.config/gtk-3.0/gtk.css <<-EOF
+.header-bar.default-decoration {
+ padding-top: 3px;
+ padding-bottom: 3px;
+ font-size: 0.8em;
+}
+
+.header-bar.default-decoration .button.titlebutton {
+ padding: 0px;
+}
+EOF
+
 echo 'Is now possible to customize the ubuntu GUI'
