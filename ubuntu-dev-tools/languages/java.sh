b76b66e00947e924c08026e4a82c838145947b97
Jeferson Cruz <jeferson.hck@gmail.com>
2018-07-16 11:00:36 -0300

    Update java.sh

--- ubuntu-dev-tools/languages/java.sh
+++ ubuntu-dev-tools/languages/java.sh
@@ -2,5 +2,5 @@
 
 echo "Installing and configuring Java 8..."
 sudo add-apt-repository ppa:webupd8team/java &&
-sudo apt-get update
+sudo apt-get update &&
 sudo apt-get install -y oracle-java8-installer oracle-java8-set-default
