ee7da57fa3ae9e67477a5efde1d66a71e29707e8
Jeferson Cruz <dev@blackfishlabs@gmail.com>
2018-12-24 13:50:13 -0300

    Fix install umake

--- ubuntu-dev-tools/tools/ubuntumake.sh
+++ ubuntu-dev-tools/tools/ubuntumake.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-echo "Installing Ubuntu Make..."
-
-sudo add-apt-repository -y ppa:lyzardking/ubuntu-make &&
-sudo apt-get update &&
-sudo apt-get install ubuntu-make
-
-echo "Ubuntu Make is done!"
+snap install ubuntu-make --classic
