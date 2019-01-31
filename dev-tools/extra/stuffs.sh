ca6fddcb01110bf96354a5d0c16a8499c89c229a
Jeferson Cruz <jeferson.hck@gmail.com>
2019-01-31 14:51:21 -0300

    Update stuffs.sh

--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-sudo snap install blender --classic && 
-sudo snap install slack --classic &&
+sudo apt install ttf-mscorefonts-installer && 
+sudo snap install slack --classic && 
 sudo snap install skype --classic && 
 sudo snap install htop && 
 sudo snap install postman && 
-sudo snap install 0ad
+sudo snap install gitkraken && 
+sudo snap install heroku --classic && 
+sudo snap install obs-studio && 
+sudo snap install 0ad && 
+wget https://download.virtualbox.org/virtualbox/6.0.4/VirtualBox-6.0.4-128413-Linux_amd64.run -O virtualbox.run &&
+chmod +x virtualbox.run &&
+sudo ./virtualbox.run
