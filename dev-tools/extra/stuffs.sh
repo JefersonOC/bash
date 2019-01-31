c1f9399a6f36e4395a4af016cb42dee438f0145a
Jeferson Cruz <jeferson.hck@gmail.com>
2019-01-31 15:03:42 -0300

    Update stuffs.sh

--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -7,8 +7,10 @@ sudo snap install htop &&
 sudo snap install postman && 
 sudo snap install gitkraken && 
 sudo snap install heroku --classic && 
+sudo apt install maven && 
 sudo snap install obs-studio && 
 sudo snap install 0ad && 
+flatpak install flathub com.dropbox.Client && 
 wget https://download.virtualbox.org/virtualbox/6.0.4/VirtualBox-6.0.4-128413-Linux_amd64.run -O virtualbox.run &&
 chmod +x virtualbox.run &&
 sudo ./virtualbox.run
