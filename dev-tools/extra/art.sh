b2fff66e8ac882e765035f3a3ca7e4c84544bae1
Jeferson Cruz <dev.blackfishlabs@gmail.com>
2019-02-02 19:49:06 -0300

    Update bash

--- dev-tools/extra/art.sh
+++ dev-tools/extra/art.sh
@@ -1,6 +1,5 @@
 sudo apt install -y gimp &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/doctormo/GimpPs/master/tools/install.sh)" &&
 flatpak install flathub org.mypaint.MyPaint &&
-flatpak install flathub org.kde.krita &&
 sudo snap install blender --classic && 
 sudo snap install inkscape
--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -11,5 +11,4 @@ sudo snap install winds &&
 flatpak install flathub com.dropbox.Client && 
 wget https://download.virtualbox.org/virtualbox/6.0.4/VirtualBox-6.0.4-128413-Linux_amd64.run -O virtualbox.run &&
 chmod +x virtualbox.run &&
-sudo ./virtualbox.run &&
-sudo snap install 0ad
+sudo ./virtualbox.run
