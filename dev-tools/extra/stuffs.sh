3e4677856bdf77d3d8bbd4a2cb096bef6156c48c
Jeferson Cruz <jeferson.hck@gmail.com>
2019-02-01 21:35:18 -0300

    Update stuffs.sh

--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -12,4 +12,4 @@ flatpak install flathub com.dropbox.Client &&
 wget https://download.virtualbox.org/virtualbox/6.0.4/VirtualBox-6.0.4-128413-Linux_amd64.run -O virtualbox.run &&
 chmod +x virtualbox.run &&
 sudo ./virtualbox.run &&
-sudo snap install 0ad && 
+sudo snap install 0ad
