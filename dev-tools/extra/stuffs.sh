e67c8330c8b4c19540269dac2832f4facec4be8a
Jeferson Cruz <jeferson.hck@gmail.com>
2019-05-06 13:02:36 -0400

    Update stuffs.sh

--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -1,13 +1,9 @@
 sudo apt install ttf-mscorefonts-installer && 
-sudo snap install slack --classic && 
-sudo snap install skype --classic && 
 sudo snap install htop && 
 sudo snap install postman && 
 sudo snap install gitkraken && 
 sudo snap install heroku --classic && 
 sudo apt install maven && 
-sudo snap install obs-studio && 
-sudo snap install winds && 
 flatpak install flathub com.dropbox.Client && 
 wget https://download.virtualbox.org/virtualbox/6.0.4/VirtualBox-6.0.4-128413-Linux_amd64.run -O virtualbox.run &&
 chmod +x virtualbox.run &&
