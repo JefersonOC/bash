e8e20ca16b7370b0eb507f8dd16cf1a058e675ed
Jeferson Cruz <jeferson.hck@gmail.com>
2019-05-06 14:54:05 -0400

    Update java.sh

--- dev-tools/languages/java.sh
+++ dev-tools/languages/java.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Installing and configuring Java 8..."
-sudo add-apt-repository ppa:webupd8team/java &&
+sudo sudo add-apt-repository ppa:linuxuprising/java &&
 sudo apt-get update &&
-sudo apt-get install -y oracle-java8-installer oracle-java8-set-default
+sudo apt-get install -y oracle-java12-installer oracle-java12-set-default
