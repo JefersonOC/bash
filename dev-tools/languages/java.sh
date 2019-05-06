575d8be6dee59008710e763deabe81d449af3651
Jeferson Cruz <jeferson.hck@gmail.com>
2019-05-06 14:54:25 -0400

    Update java.sh

--- dev-tools/languages/java.sh
+++ dev-tools/languages/java.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing and configuring Java 8..."
+echo "Installing and configuring Java 12..."
 sudo sudo add-apt-repository ppa:linuxuprising/java &&
 sudo apt-get update &&
 sudo apt-get install -y oracle-java12-installer oracle-java12-set-default
