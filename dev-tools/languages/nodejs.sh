2438b4c0f1e2b0f853917a44192b072c625c6608
Jeferson Cruz <jeferson.hck@gmail.com>
2019-12-01 11:11:31 -0400

    Update nodejs.sh

--- dev-tools/languages/nodejs.sh
+++ dev-tools/languages/nodejs.sh
@@ -2,38 +2,10 @@
 
 echo "Installing nodejs and common frontend development tools..."
 
-# download nvm
-export NVM_DIR="$HOME/.nvm" && (
-  git clone https://github.com/creationix/nvm.git "$NVM_DIR"
-  cd "$NVM_DIR"
-  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
-) && \. "$NVM_DIR/nvm.sh"
-
-# set variables
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
-
-# update .bashrc
-# cat << 'END' >> ~/.bashrc
-
-# update .zshrc
-cat << 'END' >> ~/.zshrc
-
-# nvm settings:
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-
-END
-
-# install node/npm
-nvm install stable
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+sudo apt-get install nodejs
 
 # install npm packages
-npm install -g jspm
-npm install -g gulp
-npm install -g grunt-cli
-npm install -g bower
 npm install http-server -g
 npm install -g @angular/cli
 
