83aeee8e8afe74f65e49dd1fbae1b656f8c733ba
Jeferson Cruz <jeferson.hck@gmail.com>
2019-12-01 12:02:41 -0400

    Update nodejs.sh

--- dev-tools/languages/nodejs.sh
+++ dev-tools/languages/nodejs.sh
@@ -2,10 +2,13 @@
 
 echo "Installing nodejs and common frontend development tools..."
 
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install nodejs
 
-# install npm packages
 npm install http-server -g
 npm install -g @angular/cli
 
