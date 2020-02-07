d58c312d286ec9d5ab9aed33be264c216a015a44
Jeferson Cruz <jeferson.hck@gmail.com>
2020-02-07 13:53:00 -0400

    Update stuffs.sh

--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -1,3 +1,7 @@
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" && 
+sudo dpkg -i google-chrome-stable_current_amd64.deb && 
+sudo apt-get install -y -f && 
+sudo rm google-chrome-stable_current_amd64.deb && 
 sudo apt install ttf-mscorefonts-installer && 
 sudo snap install htop && 
 sudo apt install httpie && 
