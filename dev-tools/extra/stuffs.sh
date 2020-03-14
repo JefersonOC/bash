fea57ad73de884402f0456af048a994d4cf0e9f3
Jeferson Cruz <jeferson.hck@gmail.com>
2020-03-14 10:41:28 -0400

    Update stuffs.sh

--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -5,7 +5,7 @@ sudo rm google-chrome-stable_current_amd64.deb &&
 sudo apt install ttf-mscorefonts-installer && 
 sudo snap install htop && 
 sudo apt install httpie && 
-sudo snap install postman && 
+sudo snap install insomnia && 
 sudo snap install gitkraken && 
 sudo snap install heroku --classic && 
 sudo apt install maven && 
