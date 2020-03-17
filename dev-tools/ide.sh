de47b9190ac165a15e52e3c21bc1991edc2a0cb4
Jeferson Cruz <jeferson.hck@gmail.com>
2020-03-17 08:50:39 -0400

    Update ide.sh

--- dev-tools/ide.sh
+++ dev-tools/ide.sh
@@ -2,5 +2,5 @@
 
 sudo snap install android-studio --classic && 
 sudo snap install intellij-idea-community --classic && 
-sudo snap install atom --classic && 
+sudo snap install code --classic && 
 sudo snap install pycharm-community --classic 
