1cf651f3c43960a8e09b0d9fc6c4d6859b312b3c
Jeferson Cruz <jeferson.hck@gmail.com>
2020-07-28 18:41:33 -0400

    Update art.sh

--- dev-tools/extra/art.sh
+++ dev-tools/extra/art.sh
@@ -1,5 +1,3 @@
-sudo apt install -y gimp &&
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/doctormo/GimpPs/master/tools/install.sh)" &&
 flatpak install flathub org.blender.Blender && 
 flatpak install flathub org.inkscape.Inkscape &&
 flatpak install flathub org.mypaint.MyPaint
