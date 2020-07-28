8fd6ebde405b04e07e2996a58b58ad34876dae9e
Jeferson Cruz <jeferson.hck@gmail.com>
2020-07-28 16:13:54 -0400

    Update art.sh

--- dev-tools/extra/art.sh
+++ dev-tools/extra/art.sh
@@ -1,4 +1,4 @@
 sudo apt install -y gimp &&
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/doctormo/GimpPs/master/tools/install.sh)" &&
-sudo snap install blender --classic && 
-sudo snap install inkscape
+flatpak install flathub org.blender.Blender && 
+flatpak install flathub org.inkscape.Inkscape
