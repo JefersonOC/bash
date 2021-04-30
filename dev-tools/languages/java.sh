a5576f178b440b43f9eef20b6dc03cfd16557353
Jeferson Cruz <jeferson.hck@gmail.com>
2021-04-30 09:00:22 -0400

    Update java.sh

--- dev-tools/languages/java.sh
+++ dev-tools/languages/java.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Installing and configuring Java 11..."
-sudo apt install openjdk-11-jdk
+echo "Installing and configuring Java 11 and Java 8..."
+sudo apt install openjdk-11-jdk && 
+sudo apt install openjdk-8-jdk
