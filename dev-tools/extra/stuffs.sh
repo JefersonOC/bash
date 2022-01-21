31ed08707657f8e9def2db029f745268fe65b0e2
Jeferson Cruz <jeferson.hck@gmail.com>
2022-01-21 13:06:19 -0400

    Update stuffs.sh

--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -1,7 +1,4 @@
-wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb" && 
-sudo dpkg -i google-chrome-stable_current_amd64.deb && 
-sudo apt-get install -y -f && 
-sudo rm google-chrome-stable_current_amd64.deb && 
+sudo snap install brave && 
 sudo apt install ttf-mscorefonts-installer && 
 sudo snap install gitkraken --classic && 
 sudo snap install postman &&
