2cc0cb070fe5856ac2678829a1ca4f8e61cdefa3
Jeferson Cruz <jeferson.hck@gmail.com>
2022-08-25 14:59:16 -0400

    Update nodejs.sh

--- dev-tools/languages/nodejs.sh
+++ dev-tools/languages/nodejs.sh
@@ -6,9 +6,10 @@ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install yarn
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install nodejs
+curl -fsSL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 npm install -g @angular/cli
+npm install -g http-server
 
 echo "Node, NPM, Angular, Yarn have been installed"
