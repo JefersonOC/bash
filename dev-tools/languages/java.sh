6b10b6ecccd02e2df05a5eac2944d6a9ecf5f408
Jeferson Cruz <jeferson.hck@gmail.com>
2023-02-11 22:17:21 -0400

    Update java.sh

--- dev-tools/languages/java.sh
+++ dev-tools/languages/java.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "Installing and configuring Java 11 and Java 8..."
-sudo apt install openjdk-11-jdk && 
+echo "Installing and configuring Java..."
 sudo apt install openjdk-8-jdk &&
+sudo apt install openjdk-11-jdk && 
 sudo apt install openjdk-17-jdk
