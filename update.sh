a7ffc33244fd3d494ba773143f67df81008dcd04
Jeferson Cruz <jeferson.hck@gmail.com>
2024-03-27 07:19:27 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,8 @@
 echo "** UP YOUR SYSTEM ** " &&
-sudo apt-get update &&
-sudo apt-get -y upgrade &&
-sudo apt-get -f install &&
-sudo apt-get autoremove &&
-sudo apt-get -y autoclean &&
-sudo apt-get -y clean
+flatpak update && 
+sudo apt update &&
+sudo apt -y upgrade &&
+sudo apt -f install &&
+sudo apt autoremove &&
+sudo apt -y autoclean &&
+sudo apt -y clean
