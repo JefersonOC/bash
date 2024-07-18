8f8eea44437a23dafa818c371254f722a63508ab
Jeferson Cruz <jeferson.hck@gmail.com>
2024-07-17 22:14:47 -0400

    Update stuffs.sh

--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -1,11 +1,7 @@
 sudo apt install ttf-mscorefonts-installer && 
-sudo snap install gitkraken --classic && 
 sudo snap install postman &&
 sudo snap install vlc &&
-sudo snap install telegram-desktop && 
 sudo apt install filezilla &&
 sudo snap install dbeaver-ce &&
-sudo snap install tradingview && 
 sudo apt install docker.io &&
-sudo snap install steam &&
-sudo snap install 0ad
+sudo snap install steam
