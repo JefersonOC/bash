01812032266a97677f24ee1d08377bcc286970c6
Jeferson Cruz <jeferson.hck@gmail.com>
2024-07-24 14:26:02 -0400

    Update stuffs.sh

--- dev-tools/extra/stuffs.sh
+++ dev-tools/extra/stuffs.sh
@@ -2,5 +2,11 @@ flatpak install flathub org.videolan.VLC &&
 flatpak install flathub org.filezillaproject.Filezilla &&
 flatpak install flathub io.dbeaver.DBeaverCommunity &&
 flatpak install flathub com.valvesoftware.Steam &&
+flatpak install flathub com.anydesk.Anydesk &&
+flatpak install flathub com.transmissionbt.Transmission &&
+flatpak install flathub io.podman_desktop.PodmanDesktop && 
+flatpak install flathub com.wps.Office &&
+flatpak install flathub com.obsproject.Studio &&
+flatpak install flathub tv.kodi.Kodi && 
 sudo apt install ttf-mscorefonts-installer && 
 sudo apt install docker.io 
