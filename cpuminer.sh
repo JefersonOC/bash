58330fba67a0e4f48007cedbeeaf128efb15bb98
Jeferson Cruz <jeferson.hck@gmail.com>
2024-07-25 18:23:29 -0400

    Update cpuminer.sh

--- cpuminer.sh
+++ cpuminer.sh
@@ -6,5 +6,5 @@ sudo apt-get install git -y &&
 git clone https://github.com/tpruvot/cpuminer-multi &&
 cd cpuminer-multi &&
 sudo ./build.sh
-./cpuminer -o stratum+tcp://solo.ckpool.org:3333 -u 1KTkE4XxG6H83SbnzS4feVNv2DfxvUzimv.worker1 -p x -a sha256d
+./cpuminer -o stratum+tcp://solo.ckpool.org:3333 -u 1KTkE4XxG6H83SbnzS4feVNv2DfxvUzimv.androidS21 -p x -a sha256d
 #cd cpuminer-multi && ./cpuminer -o stratum+tcp://ss.antpool.com:3333 -u 1KTkE4XxG6H83SbnzS4feVNv2DfxvUzimv.androidS21 -p x -a sha256d
