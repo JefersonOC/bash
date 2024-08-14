a3e14decf823aca5ec70bb9911cdd6140f0acd2b
Jeferson Cruz <jeferson.hck@gmail.com>
2024-08-14 17:48:27 -0400

    Update python.sh

--- tools/languages/python.sh
+++ tools/languages/python.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
 echo "Installing and configuring Python 3..."
-sudo apt-get -y install unzip python3-pip python3-dev build-essential libssl-dev libffi-dev xvfb && 
+sudo apt install python3 && 
+sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev pkg-config wget && 
 sudo pip3 install --upgrade pip
