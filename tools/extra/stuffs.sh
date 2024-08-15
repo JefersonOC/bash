2906cd1656709aefe88f48a4c8ebe26d0d6c67f3
Jeferson Cruz <jeferson.hck@gmail.com>
2024-08-14 21:28:48 -0400

    Update stuffs.sh

--- tools/extra/stuffs.sh
+++ tools/extra/stuffs.sh
@@ -3,9 +3,6 @@ flatpak install flathub org.filezillaproject.Filezilla &&
 flatpak install flathub io.dbeaver.DBeaverCommunity &&
 flatpak install flathub com.valvesoftware.Steam &&
 flatpak install flathub com.anydesk.Anydesk &&
-flatpak install flathub com.transmissionbt.Transmission &&
-flatpak install flathub io.podman_desktop.PodmanDesktop && 
-flatpak install flathub com.wps.Office &&
 flatpak install flathub com.obsproject.Studio &&
 flatpak install flathub tv.kodi.Kodi && 
 flatpak install flathub org.duckstation.DuckStation && 
