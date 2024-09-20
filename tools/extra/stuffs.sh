a17027d90ad04bcaf18f65afcd1871abcf8c517c
Jeferson Cruz <jeferson.hck@gmail.com>
2024-09-20 07:59:32 -0400

    Update stuffs.sh

--- tools/extra/stuffs.sh
+++ tools/extra/stuffs.sh
@@ -2,7 +2,6 @@ flatpak install flathub org.videolan.VLC &&
 flatpak install flathub org.filezillaproject.Filezilla &&
 flatpak install flathub io.dbeaver.DBeaverCommunity &&
 flatpak install flathub com.valvesoftware.Steam &&
-flatpak install flathub com.anydesk.Anydesk &&
 flatpak install flathub com.obsproject.Studio &&
 flatpak install flathub tv.kodi.Kodi && 
 flatpak install flathub org.duckstation.DuckStation && 
