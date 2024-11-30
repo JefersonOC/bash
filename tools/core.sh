b7f40c32768a92cd758ed5fc154116d668fe5821
Jeferson Cruz <jeferson.hck@gmail.com>
2024-11-30 17:14:17 -0400

    Update core.sh

--- tools/core.sh
+++ tools/core.sh
@@ -1,54 +1,54 @@
-#!/usr/bin/env bash
-
 echo "Installing core development libraries (a lot of stuff :P)..."
-sudo apt install flatpak && 
-flatpak remote-add --if-not-exists flathub "https://flathub.org/repo/flathub.flatpakrepo" && 
-flatpak install flathub com.google.Chrome && 
-flatpak install flathub com.opera.Opera && 
-sudo apt-get install -y make \
-                    build-essential \
-                    software-properties-common \
-                    libssl-dev \
-                    libffi-dev \
-                    zlib1g-dev \
-                    libbz2-dev \
-                    libreadline-dev \
-                    libsqlite3-dev \
-                    llvm \
-                    libncurses5-dev \
-                    libncursesw5-dev \
-                    unixodbc \
-                    unixodbc-dev \
-                    freetds-bin \
-                    freetds-dev \
-                    tdsodbc \
-                    alien \
-                    apt-transport-https \
-                    ca-certificates \
-                    git \
-                    automake \
-                    autoconf \
-                    pkg-config \
-                    libgtk-3-dev \
-                    libtiff5-dev \
-                    libjpeg8-dev \
-                    zlib1g-dev \
-                    libfreetype6-dev \
-                    liblcms2-dev \
-                    libwebp-dev \
-                    vim \
-                    curl \
-                    nano \
-                    members \
-                    unrar \
-                    net-tools \
-                    p7zip-full \
-                    zsh \
-                    snapd \
-                    tilix \
-                    ffmpeg
 
-# enable syntax highlighting for all the available languages
+# Install Snap and essential packages
+sudo apt-get install -y snapd \
+                        make \
+                        build-essential \
+                        software-properties-common \
+                        libssl-dev \
+                        libffi-dev \
+                        zlib1g-dev \
+                        libbz2-dev \
+                        libreadline-dev \
+                        libsqlite3-dev \
+                        llvm \
+                        libncurses5-dev \
+                        libncursesw5-dev \
+                        unixodbc \
+                        unixodbc-dev \
+                        freetds-bin \
+                        freetds-dev \
+                        tdsodbc \
+                        alien \
+                        apt-transport-https \
+                        ca-certificates \
+                        git \
+                        automake \
+                        autoconf \
+                        pkg-config \
+                        libgtk-3-dev \
+                        libtiff5-dev \
+                        libjpeg8-dev \
+                        zlib1g-dev \
+                        libfreetype6-dev \
+                        liblcms2-dev \
+                        libwebp-dev \
+                        vim \
+                        curl \
+                        nano \
+                        members \
+                        unrar \
+                        net-tools \
+                        p7zip-full \
+                        zsh \
+                        tilix \
+                        ffmpeg
+
+# Install Snap packages
+sudo snap install google-chrome &&
+sudo snap install opera &&
+
+# Enable syntax highlighting for nano
 find /usr/share/nano/ -iname "*.nanorc" -exec echo include {} \; >> ~/.nanorc
 
 echo "A lot of libraries have been installed for you :)"
