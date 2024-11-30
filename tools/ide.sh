5fa456ae1aa39d806a1b9448e5d2aebf13c59953
Jeferson Cruz <jeferson.hck@gmail.com>
2024-11-30 17:13:25 -0400

    Update ide.sh

--- tools/ide.sh
+++ tools/ide.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-flatpak install flathub com.google.AndroidStudio && 
-flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community && 
-sudo snap install code --classic && 
+sudo snap install android-studio --classic &&
+sudo snap install intellij-idea-community --classic &&
+sudo snap install code --classic &&
 sudo apt install maven
