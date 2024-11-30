42d5de07d556c1f963ee6654105d5288c0134fff
Jeferson Cruz <jeferson.hck@gmail.com>
2024-11-30 17:16:46 -0400

    Update python.sh

--- tools/languages/python.sh
+++ tools/languages/python.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
+set -e # Exit immediately if a command exits with a non-zero status
+set -o pipefail # Exit if any command in a pipeline fails
+
 echo "Installing and configuring Python 3..."
-sudo apt install python3 && 
-sudo apt install build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev pkg-config wget
+
+# Update package list
+echo "Updating package list..."
+sudo apt-get update
+
+# Install Python 3 and necessary development tools and libraries
+echo "Installing Python 3 and required development dependencies..."
+sudo apt-get install -y \
+    python3 \
+    build-essential \
+    zlib1g-dev \
+    libncurses5-dev \
+    libgdbm-dev \
+    libnss3-dev \
+    libssl-dev \
+    libreadline-dev \
+    libffi-dev \
+    pkg-config \
+    wget
+
+echo "Python 3 and development dependencies have been successfully installed."
+
