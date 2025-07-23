2212e0a91dca6c9870bdff5a14d00d3d1364842d
Jeferson Cruz <jeferson.hck@gmail.com>
2025-07-23 14:28:02 -0400

    Update stuffs.sh

--- tools/extra/stuffs.sh
+++ tools/extra/stuffs.sh
@@ -2,9 +2,7 @@ sudo snap install vlc &&
 sudo snap install filezilla --beta &&
 sudo snap install dbeaver-ce &&
 sudo snap install obs-studio &&
-sudo snap install kodi &&
 sudo snap install duckstation-emulator &&
-sudo snap install spotify &&
 sudo snap install steam &&
 sudo apt install ttf-mscorefonts-installer &&
 sudo apt install docker.io
